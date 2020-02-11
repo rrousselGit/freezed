2b2e29f0a06e1f7227b7aa867786d3f35569b060
Remi Rousselet <darky12s@gmail.com>
2020-02-11 17:56:02 +0000

    update header

--- lib/builder.dart
+++ lib/builder.dart
@@ -5,9 +5,9 @@ import 'src/freezed_generator.dart';
 
 /// Builds generators for `build_runner` to run
 Builder freezed(BuilderOptions options) {
-  return PartBuilder(
-    [FreezedGenerator()],
-    '.freezed.dart',
-    header: '// ignore_for_file: deprecated_member_use_from_same_package',
-  );
+  return PartBuilder([FreezedGenerator()], '.freezed.dart',
+      header: '''
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: deprecated_member_use_from_same_package
+    ''');
 }
