04378164935267758400bf5b206e1bb7d6524524
Remi Rousselet <darky12s@gmail.com>
2020-03-15 13:35:31 +0000

    Start concrete impl

---
+++ packages/freezed/test/concrete_test.dart
@@ -0,0 +1,17 @@
+// TODO: can extends a class
+// TODO: must have only a single concrete ctor
+// TODO: the single default constructor must be `MyCLass._`
+// TODO: if concrete ctor is not const, does not generate const
+// TODO: if default ctor is const and factory is const, generates const
+// TODO: does not allow mutable members
+// TODO: can use mixin on main class without having an extends
+// TODO: Freezed does not override concrete members
+// TODO: debugFillProperties still works if base class is Diagnosticable
+// TODO: concrete constructor cannot have parameters
+// TODO: must have a factory constructor next to the concrete constructor
+// TODO: can use `@With` to have union-specific interfaces
+// TODO: `@With` has the same rules as previously mentionned
+
+void main() {
+  
+}
