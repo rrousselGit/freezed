a939e0a85501421d0b39259dba5267e16cd1a259
mhmdanas <32234660+mhmdanas@users.noreply.github.com>
2020-03-24 00:23:18 +0300

    Ignore constant_identifier_names lint rule

--- packages/freezed/lib/builder.dart
+++ packages/freezed/lib/builder.dart
@@ -8,6 +8,6 @@ Builder freezed(BuilderOptions options) {
   return PartBuilder([FreezedGenerator()], '.freezed.dart',
       header: '''
 // GENERATED CODE - DO NOT MODIFY BY HAND
-// ignore_for_file: deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named
+// ignore_for_file: deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, constant_identifier_names
     ''');
 }
