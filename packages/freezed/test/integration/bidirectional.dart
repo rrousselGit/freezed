58ecb75451c259fe17f758eb5d0d346562154782
David Wimmer <david.wimmer@ffuf.de>
2020-05-19 18:04:53 +0200

    added bidirectional_test.dart

--- packages/freezed/test/integration/bidirectional.dart
+++ packages/freezed/test/integration/bidirectional.dart
@@ -4,18 +4,18 @@ part 'bidirectional.freezed.dart';
 
 @freezed
 abstract class Person with _$Person {
-  factory Person(
+  factory Person({
     String name,
     int age,
-    List<Appointment> appointments,
-  ) = _Person;
+    Appointment appointment,
+  }) = _Person;
 }
 
 @freezed
 abstract class Appointment with _$Appointment {
-  factory Appointment(
+  factory Appointment({
     String title,
     DateTime date,
     Person creator,
-  ) = _Appointment;
+  }) = _Appointment;
 }
