efec62bb67635767fc646c3b8c53b2c39ee25bbf
Remi Rousselet <darky12s@gmail.com>
2020-11-08 18:14:19 +0000

    log

--- packages/freezed/lib/src/utils.dart
+++ packages/freezed/lib/src/utils.dart
@@ -4,15 +4,22 @@ import 'package:analyzer/dart/element/element.dart';
 String documentationOfParameter(ParameterElement parameter) {
   final builder = StringBuffer();
 
-  for (Token token = parameter.session
-          .getParsedLibraryByElement(parameter.library)
-          ?.getElementDeclaration(parameter)
-          ?.node
-          ?.beginToken
-          ?.precedingComments;
-      token != null;
-      token = token.next) {
-    builder.writeln(token);
+  try {
+    for (Token token = parameter.session
+            .getParsedLibraryByElement(parameter.library)
+            ?.getElementDeclaration(parameter)
+            ?.node
+            ?.beginToken
+            ?.precedingComments;
+        token != null;
+        token = token.next) {
+      builder.writeln(token);
+    }
+  } catch (err, stack) {
+    print(
+      'Failed documentationOfParameter ${parameter} ${parameter.enclosingElement} ${parameter.source}',
+    );
+    rethrow;
   }
 
   return builder.toString();
