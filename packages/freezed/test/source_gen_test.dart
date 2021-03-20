a9d72cfb30a0f06df2352141dedb98434cfe76b9
Remi Rousselet <darky12s@gmail.com>
2021-03-20 18:35:39 +0000

    rever skip source_gen_test

--- packages/freezed/test/source_gen_test.dart
+++ packages/freezed/test/source_gen_test.dart
@@ -5,7 +5,6 @@ import 'dart:async';
 import 'package:freezed/src/freezed_generator.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:source_gen_test/source_gen_test.dart';
-import 'package:test/test.dart';
 
 Future<void> main() async {
   final reader =
@@ -13,8 +12,5 @@ Future<void> main() async {
 
   initializeBuildLogTracking();
 
-  group('source_gen_src', () {
-    testAnnotatedElements<Freezed>(reader, FreezedGenerator({}));
-    // TODO(rrousselGit) unskip source_gen_test after figuring out why they throw an InconsistentAnalysisException on the CI
-  }, skip: true);
+  testAnnotatedElements<Freezed>(reader, FreezedGenerator({}));
 }
