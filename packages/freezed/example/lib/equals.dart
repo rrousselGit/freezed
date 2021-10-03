bf73916e6954ec3821700c7a2c042c93aa0b7f2c
Cas van Luijtelaar <casvanluijtelaar@hotmail.com>
2021-10-03 16:42:52 +0200

    added custom equals example

---
+++ packages/freezed/example/lib/equals.dart
@@ -0,0 +1,12 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'equals.freezed.dart';
+
+@freezed
+class Equals with _$Equals {
+  factory Equals({ String? name, int? age }) = _Equals;
+
+  @override
+  bool operator ==(Object o) => o is Equals && o.name == name;
+
+}
