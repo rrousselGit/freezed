a6644f9fe52ba3f54e500cb4ef7d7501ea75b9f2
Cas van Luijtelaar <casvanluijtelaar@hotmail.com>
2021-10-08 19:18:21 +0200

    added normal and union equality tests

--- packages/freezed/test/custom_equals_test.dart
+++ packages/freezed/test/custom_equals_test.dart
@@ -13,23 +13,4 @@ void main() {
     );
   });
 
-  test('custom equals with implements', () {
-    expect(
-      CustomEqualsWithImplements(name: 'a', id: 1),
-      CustomEqualsWithImplements(name: 'a', id: 2),
-    );
-  });
-
-  test('custom equals in unions with implements', () {
-    expect(
-      CustomEqualsWithUnionAndImplements.first(name: 'a', id: 1),
-      CustomEqualsWithUnionAndImplements.second(name: 'a'),
-    );
-
-    expect(
-      CustomEqualsWithUnionAndImplements.second(name: 'a', id: 5) ==
-          CustomEqualsWithUnionAndImplements.third(name: 'a'),
-      isFalse,
-    );
-  });
 }
