6d7fd8a1d31cea311acc02076661b7636b5307b4
norbertkozsir <kozsir.norbert@gmail.com>
2021-10-11 11:58:26 +0200

    Update test to test actual freezed typedef

--- packages/freezed/test/typedef_parameter_test.dart
+++ packages/freezed/test/typedef_parameter_test.dart
@@ -1,4 +1,6 @@
 import 'package:analyzer/dart/analysis/results.dart';
+import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/dart/element/type.dart';
 import 'package:build_test/build_test.dart';
 import 'package:test/test.dart';
 
@@ -17,5 +19,28 @@ void main() {
         as ErrorsResult;
 
     expect(errorResult.errors, isEmpty);
+  });
+
+  test('generates correct typedefs', () async {
+    final main = await resolveSources(
+      {
+        'freezed|test/integration/typedef_parameter.dart': useAssetReader,
+      },
+          (r) => r.libraries.firstWhere(
+              (element) => element.source.toString().contains('typedef_parameter')),
+    );
+
+    var freezedClass = main.topLevelElements.whereType<ClassElement>().firstWhere((element) => element.name == '_ClassWithTypedef');
+
+    var constructor = freezedClass.constructors.firstWhere((element) => element.name == '');
+
+    var firstTypedefType = constructor.parameters.first.type;
+    expect(firstTypedefType, isA<FunctionType>());
+    expect(firstTypedefType.alias!.element.name, equals('MyTypedef'));
+
+    var secondTypedefType = constructor.parameters[1].type;
+    expect(secondTypedefType, isA<FunctionType>());
+    expect(secondTypedefType.alias!.element.name, equals('ExternalTypedef'));
+
   });
 }
