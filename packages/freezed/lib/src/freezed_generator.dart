3da5665b30394c7aa91834b8b2f6d5a51b4064e0
Stefano Rodriguez <hlsroddy@gmail.com>
2021-11-15 03:45:46 +0100

    fix: remove useless import

--- packages/freezed/lib/src/freezed_generator.dart
+++ packages/freezed/lib/src/freezed_generator.dart
@@ -9,7 +9,6 @@ import 'package:freezed/src/templates/parameter_template.dart';
 import 'package:freezed/src/templates/properties.dart';
 import 'package:freezed/src/templates/prototypes.dart';
 import 'package:freezed/src/templates/tear_off.dart';
-import 'package:freezed/src/tools/type.dart';
 import 'package:freezed/src/utils.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:source_gen/source_gen.dart';
