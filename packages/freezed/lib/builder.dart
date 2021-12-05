37d3015f12f4c793d5e36b3d0acb439f86004c57
Remi Rousselet <darky12s@gmail.com>
2021-12-05 13:33:48 +0100

    Format

--- packages/freezed/lib/builder.dart
+++ packages/freezed/lib/builder.dart
@@ -5,11 +5,14 @@ import 'src/freezed_generator.dart';
 
 /// Builds generators for `build_runner` to run
 Builder freezed(BuilderOptions options) {
-  return PartBuilder([FreezedGenerator(options.config)], '.freezed.dart',
-      header: '''
+  return PartBuilder(
+    [FreezedGenerator(options.config)],
+    '.freezed.dart',
+    header: '''
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
     ''',
-      options: options);
+    options: options,
+  );
 }
