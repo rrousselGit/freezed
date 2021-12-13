571f3a53589dc2bb12b2758226109a04c94575b3
hacker1024 <hacker1024@users.sourceforge.net>
2021-12-13 16:28:00 +1100

    Ignore all lints in generated files
    Resolves #571.

--- packages/freezed/lib/builder.dart
+++ packages/freezed/lib/builder.dart
@@ -11,7 +11,7 @@ Builder freezed(BuilderOptions options) {
     header: '''
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
-// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+// ignore_for_file: type=lint
     ''',
     options: options,
   );
