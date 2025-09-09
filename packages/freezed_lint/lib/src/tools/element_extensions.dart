58b1fe92f0937460217f46a136cf8bf8914154d6
Remi Rousselet <darky12s@gmail.com>
2025-09-09 16:14:00 +0200

    Update analyzer 8

--- packages/freezed_lint/lib/src/tools/element_extensions.dart
+++ packages/freezed_lint/lib/src/tools/element_extensions.dart
@@ -1,13 +1,13 @@
-import 'package:analyzer/dart/element/element2.dart';
+import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/dart/element/type.dart';
 
 extension DartTypeExt on DartType {
   bool hasName(String name) => getDisplayString() == name;
 }
 
-extension ClassElementExt on ClassElement2 {
+extension ClassElementExt on ClassElement {
   String? constToken() {
-    if (constructors2.any((c) => c.isConst)) return 'const ';
+    if (constructors.any((c) => c.isConst)) return 'const ';
     return null;
   }
 }
