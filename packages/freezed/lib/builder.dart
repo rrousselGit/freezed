917ede008f142a88a389ecf92997f052a1a0626a
Remi Rousselet <darky12s@gmail.com>
2026-06-13 18:46:30 +0200

    Lints

--- packages/freezed/lib/builder.dart
+++ packages/freezed/lib/builder.dart
@@ -20,7 +20,8 @@ Builder freezed(BuilderOptions options) {
     header: '''
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // coverage:ignore-file
-// ignore_for_file: type=lint, type=warning
+// ignore_for_file: type=lint, type=warning, deprecated_member_use, deprecated_member_use_from_same_package
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
     ''',
     options: options,
   );
