68eeff5bdd2c6444641eb797e5d61465da898236
Remi Rousselet <darky12s@gmail.com>
2026-08-26 22:34:19 +0200

    Update example

--- packages/freezed/example/lib/diagnosticable.dart
+++ packages/freezed/example/lib/diagnosticable.dart
@@ -9,7 +9,6 @@ abstract class Example<T> with _$Example<T> {
   factory Example(int a, String b) = _Example<T>;
   factory Example.named(T c) = _Example2<T>;
 
-  @override
   late final theAnswer = 42;
 }
 
